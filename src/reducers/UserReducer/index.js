ecaac2b5982ce7251c66ad460974f957db809be1
wcfx <wcfx.dev@gmail.com>
2021-03-18 23:48:23 -0300

    feat: userReducer criado com sucesso; async-storage adicionado para armazenar o tokem

---
+++ src/reducers/UserReducer/index.js
@@ -0,0 +1,24 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const initialState = {
+  token: '',
+  user: {},
+  property: {},
+};
+
+export default (state = initialState, action = {}) => {
+  switch (action.type) {
+    case 'SET_TOKEN':
+      AsyncStorage.setItem('token', action.payload.token);
+      return { ...state, token: action.payload.token };
+
+    case 'SET_USER':
+      return { ...state, user: action.payload.user };
+
+    case 'SET_PROPERTY':
+      return { ...state, property: action.paylooad.property };
+
+    default:
+      return state;
+  }
+};
